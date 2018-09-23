ff78684fba576b7e2eec564d1b0785c24c7e10fa
ZhangZero1 <ZhangFre0@gmail.com>
2018-09-23 16:39:47 -0400

    fixed object refrence pass in DriveTrain

--- MazeTraversal/sketch_sep23a/sketch_sep23a.ino
+++ MazeTraversal/sketch_sep23a/sketch_sep23a.ino
@@ -1,9 +1,9 @@
-class motor{
+class Motor{
   public:
   int PosPin;
   int NegPin;
   bool isFlip;
-  motor(int PosPin, int NegPin, bool isFlip){
+  Motor(int PosPin, int NegPin, bool isFlip){
     this->PosPin=PosPin;
     this->NegPin=NegPin;
     this->isFlip=isFlip;
@@ -36,41 +36,44 @@ class motor{
   }
 };
 
-class drivetrain{
-  motor m1;
-  motor m2;
+class DriveTrain{
+  public:
+  Motor* m1;
+  Motor* m2;
 
-  drivetrain(motor m1, motor m2){
-   this->m1=m1;
-   this->m2=m2;
+  DriveTrain(Motor* m1,Motor* m2){
+   this->m1 = m1;
+   this->m2 = m2;
   }
 
   void goForward(){
-    m1.Forward();
-    m2.Forward();
+    m1->Forward();
+    m2->Forward();
   }
   void turnRight(){
-    m1.Forward();
-    m2.Stop();
+    m1->Forward();
+    m2->Stop();
   }
   void turnLeft(){
-    m1.Stop();
-    m2.Forward();
+    m1->Stop();
+    m2->Forward();
   }
 
   void goBackward(){
-    m1.Backward();
-    m2.Backward();
+    m1->Backwards();
+    m2->Backwards();
   }
 
   void Stop(){
-    m1.Stop();
-    m2.Stop();
+    m1->Stop();
+    m2->Stop();
   }
 };
 void setup() {
   // put your setup code here, to run once:
-  drivetrain dt(motor(5,6,true), motor(7,8,false));
+  Motor m1(5,6, true);
+  Motor m2(7,8,false);
+  DriveTrain dt(&m1, &m2);
 }
 
 void loop() {
